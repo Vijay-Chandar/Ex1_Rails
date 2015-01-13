9cc39aa158f245ff0a2af608d837136ef51de590
Vijay-Chandar <reachvijaychandar@gmail.com>
2015-01-13 13:18:46 +0530

    Deleted one column

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150113064001) do
+ActiveRecord::Schema.define(version: 20150113073637) do
 
   create_table "users", force: true do |t|
     t.string   "name"
@@ -20,7 +20,6 @@ ActiveRecord::Schema.define(version: 20150113064001) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text     "address"
-    t.string   "city"
   end
 
 end
